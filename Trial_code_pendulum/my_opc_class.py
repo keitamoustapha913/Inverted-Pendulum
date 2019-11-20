76a656de063af9fb0c58a5a0c4fc7c9ba8a47677
Mouhamed Moustapha KEITA <keitamoustapha913@gmail.com>
2019-11-20 17:37:47 +0100

    created my_opc_class to handle the collecting of the pressures values from the FESTO PLC and the txt file shows an output example from the class

--- Trial_code_pendulum/my_opc_class.py
+++ Trial_code_pendulum/my_opc_class.py
@@ -1,57 +1,90 @@
 from opcua import Client
 import time
+
+'''
 url = "opc.tcp://192.168.142.197:4840"
 
-client = Client(url)
+opc_client = Client(url)
 
-client.connect()
-print(" Client Connected ...")
+opc_client.connect()
+print(" opc_client Connected ...")
+'''
 
-def collecting_pressures():
-    global t1
-    t1 = time.perf_counter()
-    v1 = client.get_node("ns=2;s=|var|CPX-CEC-S1-V3.Application.G.v1")
-    S1 = v1.get_value()
-    t2 = time.perf_counter()
-    print( "S1 = {} of type {} was given at {} seconds".format( S1 , type(S1) , t2-t1 ) )
-
-    t3 = time.perf_counter()
-    v2 = client.get_node("ns=2;s=|var|CPX-CEC-S1-V3.Application.G.v2")
-    S2 = v2.get_value()
-    t4 = time.perf_counter()
-    print( "S2 = {} of type {} was given at {} seconds".format( S2 , type(S2) , t4-t3 ) )
-
-    t5 = time.perf_counter()
-    v3 = client.get_node("ns=2;s=|var|CPX-CEC-S1-V3.Application.G.v3")
-    S3 = v3.get_value()
-    t6 = time.perf_counter()
-    print( "S3 = {} of type {} was given at {} seconds".format( S3 , type(S3) , t6-t5 ) )
-
-    t7 = time.perf_counter()
-    v4 = client.get_node("ns=2;s=|var|CPX-CEC-S1-V3.Application.G.v4")
-    S4 = v4.get_value()
-    t8 = time.perf_counter()
-    print( "S4 = {} of type {} was given at {} seconds".format( S4 , type(S4) , t8-t7 ) )
-
-    t9 = time.perf_counter()
-    v4 = client.get_node("ns=2;s=|var|CPX-CEC-S1-V3.Application.PLC_PRG.i" )
-    i = v4.get_value()
-    t10 = time.perf_counter()
-    print( "i = {} of type {} was given at {} seconds".format( i , type(i) , t10-t9 ) )
-    
-    global t11 
-    t11 = time.perf_counter()
-    
-    print("\n")
-    print( " The total time for 5 readings = {} seconds".format( t10-t1 ) )
+class Opc_Client():
+    def __init__(self, end_point_url = "opc.tcp://192.168.142.197:4840" ):
+        self.end_point_url = end_point_url
+        self.opc_client = Client( end_point_url )
+        self.pressure_node_1 = None
+        self.pressure_node_2 = None
+        self.pressure_node_3 = None
+        self.pressure_node_4 = None
+        self.pressure_node_list = []
+        self.connect()
 
 
+    def connect(self):
+        '''
+        To connect to the server defined at end_point_url
+        '''
+        self.opc_client.connect()
+        print(" opc_client is successfully connected at {} ...".format( self.end_point_url ) )
+        list = self.get_pressure_nodes()
+        print(" List of nodes connected to ... \n \n{} \n{} \n{} \n{}\n".format( list[0] , list[1] , list[2] , list[3] ) )
 
-for loop in range(20):
-    collecting_pressures()
+    def get_pressure_nodes(self):
+        self.pressure_node_1 = self.opc_client.get_node("ns=2;s=|var|CPX-CEC-S1-V3.Application.G.v1")
+        self.pressure_node_2 = self.opc_client.get_node("ns=2;s=|var|CPX-CEC-S1-V3.Application.G.v2")
+        self.pressure_node_3 = self.opc_client.get_node("ns=2;s=|var|CPX-CEC-S1-V3.Application.G.v3")
+        self.pressure_node_4 = self.opc_client.get_node("ns=2;s=|var|CPX-CEC-S1-V3.Application.G.v4")
+        self.pressure_node_list = [ self.pressure_node_1 , self.pressure_node_2 , self.pressure_node_3 , self.pressure_node_4]
+        return self.pressure_node_list
 
-    # time.sleep(1)
+    def collecting_pressures(self):
+        '''
+        To collect all the four pressures that is output of the FESTO valves in mbar
 
-print( " The disconnect total time for 5 readings = {} seconds".format( t11-t1 ) )
+        Return list of four pressures
+        '''
+        t1 = time.perf_counter()
+        pressure_reading1 = self.pressure_node_1.get_value()
+        t2 = time.perf_counter()
+        print( "pressure_reading 1 = {} of type {} was given at {} seconds".format( pressure_reading1 , type(pressure_reading1) , t2-t1 ) )
 
-client.disconnect()
+        t3 = time.perf_counter()
+        pressure_reading2 = self.pressure_node_2.get_value()
+        t4 = time.perf_counter()
+        print( "pressure_reading 2 = {} of type {} was given at {} seconds".format( pressure_reading2 , type(pressure_reading2) , t4-t3 ) )
+
+        t5 = time.perf_counter()
+        pressure_reading3 = self.pressure_node_3.get_value()
+        t6 = time.perf_counter()
+        print( "pressure_reading 3 = {} of type {} was given at {} seconds".format( pressure_reading3 , type(pressure_reading3) , t6-t5 ) )
+
+        t7 = time.perf_counter()
+        pressure_reading4 = self.pressure_node_4.get_value()
+        t8 = time.perf_counter()
+        print( "pressure_reading 4 = {} of type {} was given at {} seconds".format( pressure_reading4 , type(pressure_reading4) , t8-t7 ) )
+        t10 = time.perf_counter()
+        
+        print("\n")
+        print( " The total time for 5 readings = {} seconds".format( t10-t1 ) )
+
+        return [ pressure_reading1 , pressure_reading2 , pressure_reading3 , pressure_reading4  ]
+
+    def send_pressures(self, pressure_values = [0 , 0 , 0 , 0 ] ):
+        if len(pressure_values) == 4:
+            for node, pressure_value in zip(self.pressure_node_list, pressure_values):
+                node.set_value(pressure_value)
+
+    def close(self):
+        self.opc_client.disconnect()
+        print( " Successfully disconnected....." )
+
+
+if __name__ == "__main__":
+    my_client = Opc_Client()
+
+    for i in range(10):
+        my_pressures_readings = my_client.collecting_pressures()
+
+    my_client.close()
